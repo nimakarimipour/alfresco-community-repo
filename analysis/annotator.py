7bddd1a06a75d56240e0231921748c8ffb0e8989
Nima Karimipour <karimipour.nima@gmail.com>
2023-12-03 13:27:09 -0800

    add annotations

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -19,7 +19,7 @@ import shutil
 from pathlib import Path
 
 VERSION = '1.3.9-LOGISIM-SNAPSHOT'
-MODULE = 'remote-api'
+MODULE = 'core'
 REPO = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
 OUT_DIR = '{}/annotator-out/{}'.format(REPO, MODULE)
 ANNOTATOR_JAR = "{}/.m2/repository/edu/ucr/cs/riple/annotator/annotator-core/{}/annotator-core-{}.jar".format(str(Path.home()), VERSION, VERSION)
@@ -35,14 +35,14 @@ def run_annotator():
     commands = []
     commands += ["java", "-jar", ANNOTATOR_JAR]
     commands += ['-d', OUT_DIR]
-    commands += ['-bc', 'cd {} && ./annotator-command.sh'.format(REPO, MODULE)]
+    commands += ['-bc', 'cd {} && ./annotator-command.sh'.format(REPO)]
     commands += ['-cp', '{}/paths.tsv'.format(OUT_DIR)]
     commands += ['-i', 'edu.ucr.Initializer']
     commands += ['-n', 'edu.ucr.cs.riple.taint.ucrtainting.qual.RUntainted']
     commands += ['-cn', 'UCRTaint']
-    commands += ["--depth", "7"]
+    commands += ["--depth", "10"]
     # Uncomment to see build output
-    commands += ['-rboserr']
+    # commands += ['-rboserr']
     # Comment to inject root at a time
     commands += ['-ch']
     # Uncomment to disable cache
