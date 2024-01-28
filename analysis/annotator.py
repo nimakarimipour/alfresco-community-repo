4a7f6547c3de6f641279f212e4ccc37b15c04bd6
Kanak Das <kdas006@ucr.edu>
2024-01-28 01:50:41 -0800

    update paths to kdm

--- analysis/annotator.py
+++ analysis/annotator.py
@@ -18,8 +18,8 @@ import os
 import shutil
 from pathlib import Path
 
-VERSION = '1.3.9-LOGISIM-SNAPSHOT'
-MODULE = 'remote-api'
+VERSION = '1.3.9-TAINT-SNAPSHOT'
+MODULE = 'core'
 REPO = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip().decode('utf-8')
 OUT_DIR = '{}/annotator-out/{}'.format(REPO, MODULE)
 ANNOTATOR_JAR = "{}/.m2/repository/edu/ucr/cs/riple/annotator/annotator-core/{}/annotator-core-{}.jar".format(str(Path.home()), VERSION, VERSION)
